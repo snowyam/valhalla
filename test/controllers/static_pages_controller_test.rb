430b2c66e48e437e430af7ec740725ae8368e346
Sy <syeung@mail.usf.edu>
2016-09-15 23:17:13 -0400

    Finish static pages and tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  
+  def setup
+    @base_title = "Valhalla"
+  end
+
+  test "should get root" do
+    get root_url
     assert_response :success
+  end
+
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "#{@base_title} | Log In or Sign Up"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "#{@base_title} | Help"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "#{@base_title} | About"
   end
 
 end
