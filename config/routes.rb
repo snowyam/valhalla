3ea3c2fe132074a0fcb61075f19a73864ec10a58
Sy <syeung@mail.usf.edu>
2016-09-21 19:41:51 -0400

    Install devise and add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
-  get  '/help', to: 'static_pages#help'
-  get  '/about', to: 'static_pages#about'
+
+  devise_for :users, controllers: { registrations: "registrations" }
+
+  as :user do
+    get '/signup', to: 'devise/registrations#new'
+  end
+
+  root 'static_pages#home'
+  get  '/help',   to: 'static_pages#help'
+  get  '/about',  to: 'static_pages#about'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
