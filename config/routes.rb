fe2448f29d9d946a4c0d9c3f142d73f9c3903fc7
Sy <syeung@mail.usf.edu>
2016-09-22 21:59:13 -0400

    Add navbar buttons and style

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
     get '/signup', to: 'devise/registrations#new'
   end
 
-  root 'static_pages#home'
   get  '/help',   to: 'static_pages#help'
   get  '/about',  to: 'static_pages#about'
   
