a18ece4df955589cca7b4f733c978023606bd1b5
Sy <syeung@mail.usf.edu>
2016-09-23 00:33:25 -0400

    Finish initial sign up page and style

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,15 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
 
   as :user do
-    get '/signup', to: 'devise/registrations#new'
+    get '/signup',    to: 'devise/registrations#new'
+    post '/signup',   to: 'devise/registrations#create'
+    get '/login',     to: 'devise/sessions#new'
+    post '/login',    to: 'devise/sessions#create'
+    delete '/logout', to: 'devise/sessions#destroy'
   end
 
-  get  '/help',   to: 'static_pages#help'
-  get  '/about',  to: 'static_pages#about'
+  get '/help',   to: 'static_pages#help'
+  get '/about',  to: 'static_pages#about'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
