21df81ff9e23da81ce5243a245c7c7ffe0046b7a
Sy <syeung@mail.usf.edu>
2016-09-27 23:30:00 -0400

    Configure custom devise error messages, forms, and login page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+    # Extend Devise's strong parameter for forms in sign up and edit.
+    def sign_up_params
+      params.require(:user).permit(:first_name, :last_name,
+                                   :email, :password,
+                                   :password_confirmation)
+    end
+
+    def account_update_params
+      params.require(:user).permit(:first_name, :last_name,
+                                   :email, :password,
+                                   :password_confirmation,
+                                   :current_password)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Validations
+  validates :first_name,  presence: true, length: { maximum: 50}
+  validates :last_name,  presence: true, length: { maximum: 50}
 end
