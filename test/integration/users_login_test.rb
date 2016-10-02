7ad917e1b1fee4a6081f031c0db142a9d65a40f3
Sy <syeung@mail.usf.edu>
2016-10-02 19:38:17 -0400

    Configure and test login system

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,14 +1,40 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = users(:example)
+  end
   
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { user: { email: "", password: "" } }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
   end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { user: { email: @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", signup_path, count: 0
+    assert_select "a[href=?]", login_path,  count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
 end
