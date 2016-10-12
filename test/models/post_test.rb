fcdee69041af97e39ddc17b77708096872e4206c
Sy <syeung@mail.usf.edu>
2016-10-11 20:53:47 -0400

    Create basic Post model

---
+++ test/models/post_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+
+  def setup
+    @user = users(:example)
+    @post = @user.posts.build(content: "Lorem ipsum")
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+
+  test "content should be present" do
+    @post.content = " "
+    assert_not @post.valid?
+  end
+
+  test "content should be at most 12000 characters" do
+    @post.content = "a" * 12001
+    assert_not @post.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal posts(:most_recent), Post.first
+  end
+
+end
