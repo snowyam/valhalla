32f1763b53854e1c4cae89a732fe4029f27bbe35
Sy <syeung@mail.usf.edu>
2016-10-24 21:39:20 -0400

    Update seeds and home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,21 @@
 User.create!(first_name:  "Valhalla",
              last_name:   "Project",
-             email:       "valhalla@example.org",
+             email:       "valhalla@example.com",
              password:              "foobar",
              password_confirmation: "foobar",
              confirmed_at: Time.zone.now)
 
-61.times do |n|
+User.create!(first_name:  "Odin",
+             last_name:   "Project",
+             email:       "odin@example.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             confirmed_at: Time.zone.now)
+
+5.times do |n|
   first_name = Faker::Name.first_name
   last_name  = Faker::Name.last_name
-  email = "example-#{n+1}@example.org"
+  email = "example-#{n+2}@example.com"
   password = "password"
   User.create!(first_name: first_name,
                last_name:  last_name,
@@ -18,39 +25,72 @@ User.create!(first_name:  "Valhalla",
                confirmed_at: Time.zone.now)
 end
 
-users = User.order(:created_at).take(6)
-20.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.posts.create!(content: content) }
+user = User.first
+user.posts.create!(content: "Welcome to Valhalla!")
+user.posts.create!(content: "Comment on some posts! Like some stuff!")
+
+user = User.find_by_id(2)
+user.posts.create!(content: "Welcome to Valhalla!")
+user.posts.create!(content: ":)")
+
+
+users = User.where(id: 3..6)
+10.times do
+  users.each do |user|
+    content = Faker::Hipster.paragraph
+    user.posts.create!(content: content)
+  end
 end
 
 # Friendships
 users = User.all
 user  = users.first
-added_friends = users[2..50]
-friend_requests = users[3..40]
+added_friends = users[1..5]
+friend_requests = users[1..5]
 added_friends.each { |friended| user.add_friend(friended) }
 friend_requests.each { |friender| friender.add_friend(user) }
 
 # Likes
-users = User.order(:created_at).take(5)
-posts = Post.order(:created_at).take(80)
+users = User.order(:created_at).take(6)
+posts = Post.all
 posts.each do |post|
   users.each { |user| post.likes.create!(user_id: user.id)}
 end
 
 # Comments
 users = User.order(:created_at).take(3)
-posts = Post.order(:created_at).take(80)
+posts = Post.order(:created_at).take(25)
 posts.each do |post|
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| post.comments.create!(user_id: user.id,
-                                            content: content) }
+  users.each do |user|
+    content = Faker::Pokemon.name
+    post.comments.create!(user_id: user.id, content: content)
+  end
 end
 
 # Profiles
-users = User.order(:created_at).take(3)
+db = User.all
+users = db[2..6]
 users.each do |user|
-  bio = Faker::Lorem.sentence
-  user.profile.update(bio: bio)
+  bio = Faker::ChuckNorris.fact
+  birthplace = Faker::Space.nebula
+  currentplace = Faker::Space.star_cluster
+  education = Faker::Educator.university
+  occupation = Faker::Space.agency_abv
+  user.profile.update(bio: bio, birthplace: birthplace,
+                      currentplace: currentplace,
+                      education: education,
+                      occupation: occupation)
 end
+
+user = User.find_by_id(1)
+user.profile.update(bio: "I'm a Rails app!",
+                    birthplace: "The Internet",
+                    currentplace: "Heroku, AWS",
+                    occupation: "Social Network")
+
+user = User.find_by_id(2)
+user.profile.update(bio: "The Allfather",
+                    birthplace: "Asgard",
+                    currentplace: "Valhalla",
+                    education: "Aesir",
+                    occupation: "Norse God of Programming")
